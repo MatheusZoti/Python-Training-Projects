6ecd4440d80fdad4357de7e8d943246c04b16a80
Zoti <matheus.a.zoti@gmail.com>
2023-06-20 23:26:59 -0300

    Rework in input usability
    Now it is possible to send any form of valid URL.

--- 04 - Subpage finder/main.py
+++ 04 - Subpage finder/main.py
@@ -1,15 +1,24 @@
 import os
 import requests
 
-url = input("Escreva a URL do site, ex: google.com.br \n")
+url = input("Escreva a URL do site:\n")
 wordlist = open("wordlist.txt")
 
 for item in wordlist:
-  item = item.strip()
-  subpage = f"https://{url}/{item}"
-  r = requests.get(subpage)
+    item = item.strip()
 
-  if r.status_code == 200:
-    print(f"               A pagina '{url}/{item}' existe! \n")
-  else:
-    continue
+    if "http://" not in url and "https://" not in url:
+        subpage = f"https://{url}/{item}"
+    else:
+        if "/" not in url:
+            subpage = f"{url}/{item}"
+        else:
+            subpage = f"{url}{item}"
+
+    try:
+        r = requests.get(subpage)
+        if r.status_code == 200:
+            print(f"A página '{subpage}' existe!\n")
+    except requests.exceptions.RequestException as e:
+        print(f"Ocorreu um erro ao conectar à página '{subpage}': {e}\n")
+        continue
